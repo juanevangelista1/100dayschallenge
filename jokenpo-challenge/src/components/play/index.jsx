ef9598fdbe2f6b62b3d5f74b89a9a80bf20e4e47
Juan Evangelista <juan.evangelista.nascimentoo@gmail.com>
2025-11-23 17:03:24 -0300

    feat: add Play component for user to select game moves

---
+++ jokenpo-challenge/src/components/play/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export function Play({ onSelectChoice }) {
+	const choices = [
+		{ name: 'Rock ✊🏻', value: 'rock', color: 'bg-red-500' },
+		{ name: 'Paper 🤚🏻', value: 'paper', color: 'bg-green-500' },
+		{ name: 'Scissors ✌🏻', value: 'scissors', color: 'bg-blue-500' },
+	];
+
+	return (
+		<div className='flex flex-col items-center p-8'>
+			<h2 className='text-3xl font-bold mb-10'>Choose Your Move</h2>
+			<div className='flex justify-center w-full gap-[10px] my-10 mx-auto'>
+				{choices.map((choice) => (
+					<button
+						key={choice.value}
+						onClick={() => onSelectChoice(choice.value)}
+						className={`p-8 rounded-full text-white text-2xl font-semibold shadow-xl transition-transform hover:scale-105 ${choice.color}`}>
+						{choice.name}
+					</button>
+				))}
+			</div>
+		</div>
+	);
+}
